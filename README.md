28b42a9d66d442434a5e691a47fc050f1b101e71
Jin Gan <ganjinshuai@outlook.com>
2025-02-28 11:37:54 -0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MyFirst
+<<<<<<< HEAD
 C language 
+=======
+>>>>>>> b324544 (first commit)
---
+++ test.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  MyFirst
+//
+//  Created by jinshuai gan on 2025-02-04.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
+
+
